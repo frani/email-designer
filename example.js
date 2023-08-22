e00960437b7c34e1303e87872cff25b45f523ec4
frani <franinnocenti@gmail.com>
2023-08-22 18:16:42 -0300

    chore: add example

---
+++ example.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const emailDesigner = require("./");
+
+const text = `
+Gracias por Participar
+
+Ahora solo tenes que hacer CLICK en el siguiente boton.
+
+Saludos
+`;
+
+const HTML = emailDesigner.createHTML([
+  {
+    name: "base/sample",
+    params: {
+      name: "hola mundo",
+    },
+  },
+  {
+    name: "base/box-text",
+    params: {
+      text,
+      buttonTitle: "Click Me",
+    },
+  },
+  {
+    name: "base/box-otp",
+    params: {
+      text,
+      otp: "123 456",
+    },
+  },
+  {
+    name: "base/footer",
+    params: {
+      address: "olegario andrade 315",
+    },
+  },
+]);
+
+fs.writeFileSync("./example.html", HTML);
