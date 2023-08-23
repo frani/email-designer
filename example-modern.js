cce6ad3875f6254bf1815fbdfcb4dd24b69dc40f
frani <franinnocenti@gmail.com>
2023-08-23 00:46:23 -0300

    setup modern example

--- example-modern.js
+++ example-modern.js
@@ -17,12 +17,49 @@ const HTML = emailDesigner.createHTML(
     },
     {
       name: "modern/box/top",
-      params: {},
     },
     {
       name: "modern/box/text",
       params: {
         text: "a ver che si pongo esto que puede pasar ?",
+      },
+    },
+    {
+      name: "modern/box/text",
+      params: {
+        text: "a ver che si pongo esto que puede pasar ?",
+      },
+    },
+    {
+      name: "modern/box/bottom",
+    },
+    {
+      name: "modern/box/top",
+    },
+    {
+      name: "modern/box/text",
+      params: {
+        text: "a ver che si pongo esto que puede pasar ?",
+        style:
+          "padding: 100px 0px 0px 20px; font-family: arial; font-weight: 600;",
+      },
+    },
+    {
+      name: "modern/box/bottom",
+    },
+    {
+      name: "modern/box/top",
+    },
+    {
+      name: "modern/box/text",
+      params: {
+        text: "a ver che si pongo esto que puede pasar ?",
+      },
+    },
+    {
+      name: "modern/box/otp",
+      params: {
+        otp: "123654",
       },
     },
     {
@@ -35,12 +72,17 @@ const HTML = emailDesigner.createHTML(
     },
     {
       name: "modern/footer/social-icons",
-      params: {},
+      params: {
+        whatsapp: "https://wa.me/+549",
+        instagram: "https://instagram.com/palta-app",
+        facebook: "https://fb.com/palta-app",
+      },
     },
     {
       name: "modern/footer/address",
       params: {
-        address: "olegario andrade 315",
+        address:
+          "Palta SAS, Campus Olegario, Olegario Andrade 315, Mendoza, M5500 BDP",
       },
     },
   ],
